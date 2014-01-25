ad6eaa2dee7a4cb8963ac36fb316142458b58e1b
Drew Crawford <drew@sealedabstract.com>
2014-01-25 02:33:40 -0600

    bool test coverage

--- caffeine-iosTests/MsgPackTests.m
+++ caffeine-iosTests/MsgPackTests.m
@@ -41,23 +41,33 @@ XCTAssertEqualObjects(input,reserialized)
 
     
     
-    //fixint
     NSNumber *test = [NSNumber numberWithChar:2];
     NSMutableData *data = nil;
     NSNumber *reserialized = nil;
     NSData *knownOutputData = nil;
     int bytesRead = 0;
     
-
+    //fixint
     TEST(test, {0x02});
     
     //fixint negative
     test = [NSNumber numberWithChar:-2];
     TEST(test,{0xfe});
     
+    //bool
+    //be advised, ObjC pretty much treats BOOL as 0,1.  So if you were expecting MsgPack's actual bool (c3), be prepared to be surprised.
+    TEST([NSNumber numberWithBool:YES],{0x01});
+    TEST(@NO,{0x00});
     
-    test = [NSNumber numberWithBool:YES];
-    data = [NSMutableData data];
+    //however, it's easy to verify that the reverse works as expected
+    NSData *oobdata = [NSData dataWithBytes:(unsigned char[]){0xc3} length:1];
+    bytesRead = 0;
+    NSNumber *yes = [NSNumber unMsgPackFromData:oobdata bytesRead:&bytesRead];
+    XCTAssertTrue(yes.boolValue, @"Not true?");
+    oobdata = [NSData dataWithBytes:(unsigned char[]){0xc2} length:1];
+    bytesRead = 0;
+    NSNumber *no = [NSNumber unMsgPackFromData:oobdata bytesRead:&bytesRead];
+    XCTAssertFalse(no.boolValue, @"Not no?");
     
     
     
