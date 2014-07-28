53dc733f95e5371b8804d6c1810f6d97c063856d
Drew Crawford <drew@sealedabstract.com>
2014-07-28 02:33:35 -0500

    modifying build behavior

--- analyze.sh
+++ analyze.sh
@@ -6,31 +6,13 @@ set -e
 set -x
 source ~/.profile
 DESTINATION="platform=iOS Simulator,name=iPhone Retina (4-inch),OS=latest"
-SCHEME="caffeine-ios"
+SCHEME="caffeine-ios-unit"
 WORKSPACE="caffeine-ios.xcodeproj/project.xcworkspace"
-set +e
-echo "server install is"
-cat CAFFEINE_SERVER
-SERVER_HOSTNAME=`cat CAFFEINE_SERVER | grep -oE '^[A-Za-z\.]+'`
-SERVER_PORT=`cat CAFFEINE_SERVER | grep -oE '[0-9]+$'`
-echo $SERVER_HOSTNAME
-echo $SERVER_PORT
-nc -z $SERVER_HOSTNAME $SERVER_PORT
-if [ $? -ne 0 ]; then
-	echo "Server isn't running.  Run it before analysis."
-	exit 27
-fi
-set -e
-
-#this works around an unfiled issue
-rm -rf ~/Library/Developer/Xcode/DerivedData
 
 #analyze
 xcodebuild -workspace "$WORKSPACE" -scheme "$SCHEME" -sdk iphonesimulator clean analyze | teamcitify
 #test
-ZEROMQ_URL="tcp://"$(cat CAFFEINE_SERVER)
-xcodebuild -workspace "$WORKSPACE" -scheme "$SCHEME" -destination "$DESTINATION" GCC_PREPROCESSOR_DEFINITIONS="\$(value) CAFFEINE_OVERRIDE_URL=@\\\"${ZEROMQ_URL}\\\"" test | teamcitify
-#sonar-runner
-#work around rdar://15489119
-rm -rf ~/Library/Developer/Xcode/DerivedData
-bash run-sonar.sh -v
+xcodebuild -workspace "$WORKSPACE" -scheme "$SCHEME" -destination "$DESTINATION" test | teamcitify
+#production 
+xcodebuild -workspace "$WORKSPACE" -scheme "$SCHEME" -destination "generic/platform=iOS" -derivedDataPath "built" build | teamcitify
+echo "##teamcity[publishArtifacts 'built/Build/Products/Release-iphoneos']" #configure products
---
+++ caffeine-iosIntegration/caffeine_iosIntegration.m
@@ -0,0 +1,34 @@
+//
+//  caffeine_iosIntegration.m
+//  caffeine-iosIntegration
+//
+//  Created by Drew Crawford on 7/28/14.
+//  Copyright (c) 2014 DrewCrawfordApps. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface caffeine_iosIntegration : XCTestCase
+
+@end
+
+@implementation caffeine_iosIntegration
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+//    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
